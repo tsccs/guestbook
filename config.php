527f8c3b15763e2f3dc964fdef4c83832cc1dd27
Travis Smith <54687941+tsccs@users.noreply.github.com>
2024-09-24 18:00:23 -0400

    Update config.php
    Added $locationlist option.

--- config.php
+++ config.php
@@ -6,7 +6,8 @@ $perpagemax = 10; // How many guests are shown on each page.
 $adminrequired = false; // Is admin login required to view guest list.
 $guestnonadminmax = 0; // Show this many recent guests, only if admin is required (above).
 $keeplocation = true; //Keep location filled after guestbook add.
-$hash_pass = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"; // Hash of password 1234
+$locationlist = ''; //Location List, separated by comma or semicolon. (Optional)
+$hash_pass = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"; // sha256 Hash of password 1234
 $redirect = '';
 
 ?>
